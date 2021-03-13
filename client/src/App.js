be5eca5e77a57982e6a4f39a2d78c0cd2a0e1e5f
devon <dleardini12@gmail.com>
2021-03-13 12:12:10 -0800

    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,14 @@ import Jumbotron from "./component/Jumbotron";
 class App extends Component {
   render() {
     return (
-
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <Jumbotron>
+        <h1>Parcel</h1>
+        <p>ship more for less</p>
+      </Jumbotron>
       </div>
-    );
-  }
+    )
+  }  
 }
 
 export default App;
