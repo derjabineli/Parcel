03fd4690a4802238572352d4358d462436428f72
Mike Derjabin <m.derjabin@gmail.com>
2021-03-17 18:39:33 -0700

    Mike's Push

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,49 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
 import "./App.css";
-import Jumbotron from "./component/Jumbotron";
+import {Button, Navbar, Nav, NavDropdown, Form, FormControl} from 'react-bootstrap'
 
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-      <Jumbotron>
-        <h1>Parcel</h1>
-        <p>ship more for less</p>
-      </Jumbotron>
-      </div>
-    )
-  }  
+function App() {
+  return (
+    <>
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+    <div>Test</div>
+    <Button>This is a button</Button>
+    </>
+  )
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//       <Jumbotron>
+//         <h1>parcel.</h1>
+//         <p>ship more for less</p>
+//       </Jumbotron>
+//       </div>
+//     )
+//   }  
+// }
 
 export default App;
